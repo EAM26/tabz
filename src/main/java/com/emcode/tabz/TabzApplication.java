4c073fa131e82ed2126e64eac1c081554a98a64b
emile <emuller2607@gmail.com>
2026-05-18 10:13:26 +0200

    First commit

---
+++ src/main/java/com/emcode/tabz/TabzApplication.java
@@ -0,0 +1,13 @@
+package com.emcode.tabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emcode/tabz/TabzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emcode.tabz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

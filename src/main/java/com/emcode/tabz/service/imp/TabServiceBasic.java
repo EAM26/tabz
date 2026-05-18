e681afe2adc73ee485cde93c1f40aca044221ddf
emile <emuller2607@gmail.com>
2026-05-18 21:48:00 +0200

    Implement store functionality with LocalFileStorage

--- src/main/java/com/emcode/tabz/service/imp/TabServiceBasic.java
+++ src/main/java/com/emcode/tabz/service/imp/TabServiceBasic.java
@@ -1,15 +1,43 @@
 package com.emcode.tabz.service.imp;
 
+import com.emcode.tabz.model.Merchant;
+import com.emcode.tabz.model.Tab;
+import com.emcode.tabz.repository.MerchantRepo;
+import com.emcode.tabz.repository.TabRepo;
+import com.emcode.tabz.service.FileStorageManager;
+import com.emcode.tabz.service.MerchantService;
 import com.emcode.tabz.service.TabService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 @Service
 public class TabServiceBasic implements TabService {
 
-    @Override
-    public String createTab(MultipartFile multipartFile, Long merchantId) {
-        String originalFileName = multipartFile.getOriginalFilename();
-        return "Service running: " + originalFileName;
+    private final FileStorageManager storageManager;
+    private final TabRepo tabRepo;
+    private final MerchantRepo merchantRepo;
+
+    public TabServiceBasic(FileStorageManager storageManager, TabRepo tabRepo, MerchantRepo merchantRepo) {
+        this.storageManager = storageManager;
+        this.tabRepo = tabRepo;
+        this.merchantRepo = merchantRepo;
     }
+
+    @Override
+    public String createTab(MultipartFile file, Long merchantId) {
+        Merchant merchant = merchantRepo.findById(merchantId).orElseThrow(()
+                -> new NoSuchElementException("No merchant found with id: " + merchantId));
+        Tab tab = new Tab();
+        tab.setMerchant(merchant);
+        Tab savedTab = tabRepo.save(tab);
+
+        return storageManager.storeFile(merchantId, savedTab.getId(), file);
+    }
+
+
+
+
 }
